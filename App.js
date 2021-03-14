181599e701d8872041dd5ce056256c5d011984dc
Ariel <arielgavrielov@icloud.com>
2021-03-14 16:08:23 +0200

    add side menu (need fixed)

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from 'react-native-elements';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button, ThemeProvider } from 'react-native-elements';
 
 export default function App() {
   return (
       <SafeAreaProvider>
         <Header
-        leftComponent={{ icon: 'menu', color: '#fff' }}
-        centerComponent={{ text: 'CareLog', style: { color: '#fff' } }}
-        rightComponent={{ icon: 'home', color: '#fff' }}
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'CareLog', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
         />
+        <ThemeProvider>
+          <Button title="Hey!" />
+        </ThemeProvider>
       </SafeAreaProvider>
   );
 }
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import { registerRootComponent } from 'expo';
-
+import React, { Component } from 'react';
+import { AppRegistry, Dimensions } from 'react-native';
+import { DrawerNavigator } from 'react-navigation';
 import App from './App';
+import SideMenu from './src/components/SideMenu/SideMenu'
+import stackNav from './src/components/SideMenu/stacknav';
 
+
+const drawernav = DrawerNavigator({
+    Item1: {
+        screen: stackNav,
+      }
+    }, {
+      contentComponent: SideMenu,
+      drawerWidth: Dimensions.get('window').width - 120,  
+  });
+  
+  AppRegistry.registerComponent('Demo', () => drawernav);
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+//registerRootComponent(App);
